13ed248f9d38fddf9877afa5274ed0edc90dc451
gpandey24in <g_pandey24in@yahoo.com>
2020-09-08 19:03:04 -0700

    Added Service

---
+++ src/app/employee/emp.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpService {
+
+  constructor(private http: HttpClient) { }
+
+  saveEmployee(EmployeeData) {
+    return this.http.post('https://localhost:44302/emp', EmployeeData);
+  }
+
+  UpdateEmployee(EmployeeData) {
+    return this.http.put('https://localhost:44302/emp', EmployeeData);
+  }
+
+  GetAllEmployee() {
+    return this.http.get('https://localhost:44302/emp');
+  }
+
+  GetEmployeeById(empId) {
+    return this.http.get('https://localhost:44302/emp' + empId);
+  }
+
+  DeleteEmployeeById(empId) {
+   return this.http.delete('https://localhost:44302/emp' + empId);
+  }
+
+}
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormControl, FormBuilder } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { EmpService } from './emp.service';
 
 
 @Component({
@@ -11,7 +12,8 @@ import { HttpClient } from '@angular/common/http';
 export class EmployeeComponent implements OnInit {
 
   employeeForm: FormGroup;
-  constructor(private fb: FormBuilder, private http:HttpClient) {
+  constructor(private fb: FormBuilder, private http: HttpClient,
+    private empService: EmpService) {
        }
 
   ngOnInit(): void {
@@ -20,12 +22,25 @@ export class EmployeeComponent implements OnInit {
       Email: [''],
       Password: ['']
     });
+
+    this.GetEmployeeData();
   }
   OnSubmit() {
     //console.log(this.employeeForm);
-    this.http.post('https://localhost:44302/emp', this.employeeForm.value).subscribe(data => {
+   // this.http.post('https://localhost:44302/emp', this.employeeForm.value)
+   //.subscribe(data => {
+     // console.log(data);
+
+
+    this.empService.saveEmployee(this.employeeForm.value).subscribe(data => {
       console.log(data);
     });
 }
+
+  GetEmployeeData() {
+    this.empService.GetAllEmployee().subscribe(data => {
+      console.log(data);
+    });
+  }
 
 }
